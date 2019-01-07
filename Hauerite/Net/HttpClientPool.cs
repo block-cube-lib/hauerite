c9940711e9beaba0648a4b6d7a68fb88c5270561
block <block@tortoise-games.com>
2019-01-07 23:09:58 +0900

    refactor: usingを整理

--- Hauerite/Net/HttpClientPool.cs
+++ Hauerite/Net/HttpClientPool.cs
@@ -1,9 +1,8 @@
 using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Linq;
 using System.Threading.Tasks;
 using Haurite.Threading;
 
